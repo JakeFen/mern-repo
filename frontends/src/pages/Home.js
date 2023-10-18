2a444e4e0cf7bb2c086212eaf375b6003c363097
Jake Fenwick <JFenwick@WQLVWT0FN2.attlocal.net>
2023-10-18 01:44:45 -0700

    ..

---
+++ frontends/src/pages/Home.js
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+const Home = () => {
+  useEffect(() => {});
+   
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+};
+
+export default Home;
