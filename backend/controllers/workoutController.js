1aa6b597587be9371549d4d6fa9a5dbebca1d6ff
Jake Fenwick <JFenwick@WQLVWT0FN2.attlocal.net>
2023-10-18 01:09:19 -0700

    ..

---
+++ backend/controllers/workoutController.js
@@ -0,0 +1,85 @@
+const Workout = require("../models/workoutModel");
+const mongoose = require("mongoose");
+
+// GET all workouts
+const getWorkouts = async (req, res) => {
+  const workouts = await Workout.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(workouts);
+};
+
+// GET single workout
+const getWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid id" });
+  }
+
+  const workout = await Workout.findById(id);
+
+  if (!workout) {
+    return res.status(404).json({ error: "No workout found" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// CREATE new workout
+const createWorkout = async (req, res) => {
+  const { title, reps, load } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+
+// DELETE workout
+const deleteWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid id" });
+  }
+
+  const workout = await Workout.findOneAndDelete({ _id: id });
+
+  if (!workout) {
+    return res.status(404).json({ error: "No workout found" });
+  }
+
+  res.status(200).json(workout);
+};
+
+// UPDATE workout
+const updateWorkout = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "Invalid id" });
+  }
+
+  const workout = await Workout.findOneAndUpdate(
+    { _id: id },
+    {
+      ...req.body,
+    }
+  );
+
+  if (!workout) {
+    return res.status(404).json({ error: "No workout found" });
+  }
+
+  res.status(200).json(workout);
+};
+
+module.exports = {
+  getWorkouts,
+  getWorkout,
+  createWorkout,
+  deleteWorkout,
+  updateWorkout,
+};
---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
+
+// Express app
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(req.menthod, req.path);
+  next();
+});
+
+// Routes
+app.use("/api/workouts", workoutRoutes);
+
+// Connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // Listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("conected to db & listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
